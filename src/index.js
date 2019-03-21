5d86536622d55915ee9f5dc03c67364586cbf14e
speedyplane2247 <36555661+speedyplane2247@users.noreply.github.com>
2019-03-21 10:41:22 -0400

    XsChat v7.0.0 - Developer Beta 3

--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
 /*
 XsChat v7.0.0 Client BootStrapper
 ---------------------------------
-Developer Beta 2
+Developer Beta 3
 ---------------------------------
 (c) speedyplane2247 2019
 */
+//var $enc = new Object();
+//var encrypt = CryptoJS.AES.encrypt
+//var decrypt = CryptoJS.AES.encrypt
 var $enc = new Object();
+if (navigator.userAgent.Contains("Firefox") != true) {
+    alert("XsChat v7 reccomends firefox for easier data transfer.")
+    $enc.doTheFox = 0
+} else {
+    $enc.doTheFox = 1
+}
+
+function init() {
+$enc.message = document.getElementById("data_1").value
+$enc.key = document.getElementBydId("key_1").value
+}
+function encrypt() {
+var encryptedAES = CryptoJS.AES.encrypt($enc.message, $enc.key);
+if (doTheFox == 1) {
+alert(encryptedAES)
+} else {
+alert("Your browser is not optimized for XsChat and the data has been printed to the console.")
+console.log(encryptedAES)
+}
+}
+function decrypt() {
+    var decryptedBytes = CryptoJS.AES.decrypt($enc.message, $enc.key);
+    if (doTheFox == 1) {
+    alert(decryptedBytes.toString(CryptoJS.enc.Utf8))
+    } else {
+        alert("Your browser is not optimized for XsChat and the data has been printed to console.")
+        console.log(decryptedBytes.toString(CryptoJS.enc.Utf8))
+    }
+}

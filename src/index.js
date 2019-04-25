d646b72f2962525ee285d90f54b0802eb18062ac
speedyplane2247 <36555661+speedyplane2247@users.noreply.github.com>
2019-04-25 12:33:13 -0400

    XsChat 8.1.2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 // XsChat 8 Client
 // (c) speedyplane2247 2019
-// Version: 8.1.1
+// Version: 8.1.2
 var $xsui = new Object()
 var $enc = new Object()
 var $xs = new Object()
-$xs.version = "XsChat 8.1.1\nEngine: 9.0.0\nUI: 2.0\nKeychain: 1.3.1"
-$xs.cver = "811"
+$xs.version = "XsChat 8.1.2\nEngine: 9.0.0\nUI: 2.0\nKeychain: 1.3.1"
+$xs.cver = "812"
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -20,7 +20,7 @@ xhttp.send();
 
 $xs.x800 = new Object()
 $xs.x800.encrypt = function (data, channel) {
-    $enc.encs1 = split("###\nBEGIN ENCRYPTED HEADER\nClient=8.1.0\nLegacy=TRUE\nEND ENCRYPTED HEADER\n###\nBEGIN ENCRYPTED MESSAGE\n###\n" + data + "\n###\nEND ENCRYPTED MESSAGE\n##LEGACY\n#XSCHAT8COMPATMODE=TRUE\n###\nEND FULL TEXT\npass***!="+channel)
+    $enc.encs1 = split("###\nBEGIN ENCRYPTED HEADER\nClient=8.1.2\nLegacy=TRUE\nEND ENCRYPTED HEADER\n###\nBEGIN ENCRYPTED MESSAGE\n###\n" + data + "\n###\nEND ENCRYPTED MESSAGE\n##LEGACY\n#XSCHAT8COMPATMODE=TRUE\n###\nEND FULL TEXT\npass***!="+channel)
     $enc.encs20 = btoa($enc.encs1)
     $enc.encs21 = btoa($enc.encs20)
     $enc.encs2 = btoa($enc.encs21)
@@ -38,7 +38,8 @@ $xs.x800.decrypt = function (data, channel) {
         if ($enc.dencs4.indexOf("pass***!="+channel) == -1) {
         alert("Invalid Password!")
         } else {
-        alert($enc.dencs4)
+        $enc.dencs5 = $enc.dencs4.split("pass***!=")
+        alert($enc.dencs5[0 ])
         }
     } catch (e) {
         alert("An error occured while decrypting. This may be because you are missing a letter, the message contains invalid characters, or a programming error. See the console for details.")
@@ -49,7 +50,7 @@ $xs.x800.decrypt = function (data, channel) {
 $xs.x711 = new Object()
 $xs.x711.encrypt = function(data) {
     try {
-        $enc.encs1 = split("###\nBEGIN ENCRYPTED HEADER\nClient=8.1.0\nLegacy=TRUE\nEND ENCRYPTED HEADER\n###\n###\nBEGIN ENCRYPTED MESSAGE\n" + data + "\n###\nEND ENCRYPTED MESSAGE\n##LEGACY\n#XSCHAT8COMPATMODE=TRUE\n###\nEND FULL TEXT")
+        $enc.encs1 = split("###\nBEGIN ENCRYPTED HEADER\nClient=8.1.2\nLegacy=TRUE\nEND ENCRYPTED HEADER\n###\n###\nBEGIN ENCRYPTED MESSAGE\n" + data + "\n###\nEND ENCRYPTED MESSAGE\n##LEGACY\n#XSCHAT8COMPATMODE=TRUE\n###\nEND FULL TEXT")
         $enc.encs2 = btoa($enc.encs1)
         alert($enc.encs2)
         // $enc.dencs2 = atob($enc.encs2)
